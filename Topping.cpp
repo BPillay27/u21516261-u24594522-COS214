943a2259e55cbef99b1c96682014918fadb2b552
Keegan <205822686+Keegan-l-star@users.noreply.github.com>
2025-08-26 19:21:24 +0200

    Implement getName and getPrice methods in Topping

--- Topping.cpp
+++ Topping.cpp
@@ -8,8 +8,9 @@ Topping::Topping(double price, std::string name) : PizzaComponent(price, name)
 }
 std::string Topping::getName()
 {
-    
+    return this->name;   
 }
 double Topping::getPrice()
 {
+    return this->price;
 }
