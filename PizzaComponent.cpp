0e22ccb27f8a349a9898eb91ce33483d50dccfd9
Keegan <205822686+Keegan-l-star@users.noreply.github.com>
2025-08-26 19:25:08 +0200

    Remove unused methods from PizzaComponent
    Removed getBaseName and getBasePrice methods.

--- PizzaComponent.cpp
+++ PizzaComponent.cpp
@@ -5,13 +5,3 @@ PizzaComponent::PizzaComponent(double price, std::string name)
     this->price = price;
     this->name = name;
 }
-
-std::string PizzaComponent::getBaseName()
-{
-    return name;
-}
-
-double PizzaComponent::getBasePrice()
-{
-    return price;
-}
