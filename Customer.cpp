8c727389e6833eca3476941f9e72d8ebcc37d94d
Keegan <205822686+Keegan-l-star@users.noreply.github.com>
2025-08-28 18:49:56 +0000

    Made change so vscode would not be angry

---
+++ Customer.cpp
@@ -0,0 +1,30 @@
+#include "Customer.h"
+//
+
+Customer::Customer():Observer(){
+    this->order=nullptr;
+}
+
+Customer::~Customer(){
+    if(order!=nullptr){
+        delete order;
+    }
+    order=nullptr;
+}
+
+void Customer::update(std::string message){
+    std::cout<<"Customer has received "<<message<<" \n and will update their known menus"<<std::endl;
+}
+
+void Customer::makeOrder(){
+    if(this->order==nullptr){
+        this->order=new Order();
+    }
+}
+
+void Customer::Proceed(){
+    if(this->order==nullptr){
+        return;
+    }
+    this->order->proceed();
+}
---
+++ Order.h
@@ -0,0 +1,14 @@
+#ifndef ORDER_h
+#define ORDER_H
+
+#include <iostream>
+//#include "ExtraCheese.h"
+//#include "BasePizza.h"
+//#include "StuffedCrust.h"
+
+class Order{
+
+
+};
+
+#endif ORDER_H
