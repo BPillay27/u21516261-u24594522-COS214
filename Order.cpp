f37fe93303d0cb1cba31901875627af3b63c00f7
Byron Pillay <byronpillay2704@gmail.com>
2025-08-29 12:38:36 +0200

    Create Order.cpp

---
+++ Order.cpp
@@ -0,0 +1,48 @@
+#include "Order.h"
+
+void Order::next()
+{
+    state->proceed();
+}
+
+bool Order::setState(Process *next)
+{
+    if (next == nullptr)
+        return false;
+
+    if (state != nullptr)
+    {
+        delete state;
+        state = nullptr;
+    }
+
+    state = next;
+    return true;
+}
+
+bool Order::addPizza(Pizza *ordering)
+{
+    if (ordering == nullptr)
+        return false;
+
+    pizzas.push_back(ordering);
+    return true;
+}
+
+Order::Order()
+{
+    state = new Pick();
+}
+
+Order::~Order()
+{
+    for (auto p : pizzas)
+    {
+        delete p;
+    }
+
+    pizzas.clear();
+
+    delete state;
+    state = nullptr;
+}
