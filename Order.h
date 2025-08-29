338b0bf358c1520f86e458f10ebfd046b6321373
Byron Pillay <byronpillay2704@gmail.com>
2025-08-29 12:37:52 +0200

    Create Order.h

---
+++ Order.h
@@ -0,0 +1,22 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include "Pizza.h"
+#include <list>
+#include "Pick.h"
+
+#include "Process.h"
+
+class Order{
+public: 
+    void next();
+    bool setState(Process* next);
+    bool addPizza(Pizza* ordering);
+    Order();
+    ~Order();
+private:
+    std::list<Pizza*> pizzas;
+    Process* state;
+};
+
+#endif
