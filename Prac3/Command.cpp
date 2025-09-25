2f7d1b307a5959cb7263e7d62e542b514a555f67
Byron Pillay <byronpillay2704@gmail.com>
2025-09-25 18:12:47 +0200

    Add files via upload

--- Prac3/Command.cpp
+++ Prac3/Command.cpp
@@ -1,11 +1,29 @@
 #include "Command.h"
 
+/**
+ * @brief This is the `Command` class constructor
+ * @details This is the constructor for the `Command` class. Takes in a `User`, `string` and `ChatRoom` and sets the member variables accordingly.
+ * 
+ * @param fromUser The user involved in the command.
+ * @param message The message being sent/logged.
+ * @param room The ChatRoom in use by the command
+ */
+
 Command::Command(Users* fromUser, std::string message, ChatRoom* room){
     // Constructor implementation
     this->fromUser = fromUser;
     this->message = message;
     this->room = room;
 }
+
+/**
+ * @brief Destructor for the `Command` Class
+ * @details This is the destructor for the `Command` class.
+ * 1. Checks if `fromUser` is `nullptr`. If not, set `fromUser` to `nullptr`.
+ * 2. Checks if `room` is `nullptr`. If not, set `room` to `nullptr`.
+ * 
+ * @note Does not free dynamically allocated memory since it is not responsible to do so.
+ */
 
 Command::~Command(){
     // Destructor implementation
@@ -17,13 +35,39 @@ Command::~Command(){
     }
 }
 
+/**
+ * @brief Constructor for thhe SendMessageCommand class
+ * @details This is the constructor for the subclass SendMessageCommand. The function calls the base class (Command) constructor and parses the appropriate values.
+ * 
+ * @param fromUser The user involved in the command.
+ * @param message The message being sent/logged.
+ * @param room The ChatRoom in use by the command.
+ */
+
 SendMessageCommand::SendMessageCommand(Users* fromUser, std::string message, ChatRoom* room) : Command(fromUser, message, room) {
     // Constructor implementation
 }
 
+/**
+ * @brief Constructor for thhe LogMessageCommand class
+ * @details This is the constructor for the subclass LogMessageCommand. The function calls the base class (Command) constructor and parses the appropriate values.
+ * 
+ * @param fromUser The user involved in the command.
+ * @param message The message being sent/logged.
+ * @param room The ChatRoom in use by the command.
+ */
+
 LogMessageCommand::LogMessageCommand(Users* fromUser, std::string message, ChatRoom* room) : Command(fromUser, message, room) {
     // Constructor implementation
 }
+
+/**
+ * @brief Implementation of the pure virual execute operation
+ * @details 
+ * 1. Checks if `fromUser` is not `nullptr`.
+ * 2. Checks if 'room' is not `nullptr'.
+ * 3. If all of the above is true, calls `room->sendMessage(this->message, this->fromUser)` which sends the message to the `ChatRoom`.
+ */
 
 void SendMessageCommand::execute() {
     if(this->fromUser != nullptr && this->room != nullptr) {
@@ -31,15 +75,31 @@ void SendMessageCommand::execute() {
     }
 }
 
+/**
+ * @brief Implementation of the pure virual execute operation
+ * @details 
+ * 1. Checks if `fromUser` is not `nullptr`.
+ * 2. Checks if 'room' is not `nullptr'.
+ * 3. If all of the above is true, calls `room->saveMessage(this->message, this->fromUser)` which saves the message to the `ChatRoom` history.
+ */
+
 void LogMessageCommand::execute() {
     if(this->fromUser != nullptr && this->room != nullptr) {
         this->room->saveMessage(this->message, this->fromUser);
     }
 }
 
+/**
+ * @brief Destructor for the SendMessageCommand class
+ */
+
 SendMessageCommand::~SendMessageCommand() {
     // Destructor implementation
 }
+
+/**
+ * @brief Destructor for the LogMessageCommand class
+ */
 
 LogMessageCommand::~LogMessageCommand() {
     // Destructor implementation
--- Prac3/Command.h
+++ Prac3/Command.h
@@ -5,6 +5,21 @@
 #include "ChatRoom.h"
 class Users; // Forward declaration to avoid circular dependency
 
+/**
+ * @defgroup Command Command Pattern
+ * @brief Collection of classes that belong to the Command Pattern
+ * @details The classes within this group are participants of a Command design pattern.
+ */
+
+/**
+ * @class Command
+ * @brief Abstract base class for the Command Pattern
+ * 
+ * @author Byron Pillay and Kiegan Lewis
+ * @date 2025-09-25
+ * @ingroup Command
+ */
+
 class Command {
     protected:
         Users* fromUser;
@@ -16,12 +31,42 @@ class Command {
         virtual ~Command();
 };
 
+/**
+ * @defgroup ConcreteCommand
+ * 
+ * @details This group contains the Concrete Command participants of the Command Pattern.
+ * **Participants:**
+ * -`SendMessageCommand`: A concrete implementation of Command for handling sending messages.
+ * -`LogMessageCommand`: A concrete implementation of Command for handling logging messages.
+ * 
+ * @ingroup Command
+ * @brief Collection of Concrete Command participants
+ */
+
+/**
+ * @class SendMessageCommand
+ * @brief Concrete command for sending a message.
+ * 
+ * @author Byron Pillay and Kiegan Lewis
+ * @date 2025-09-25
+ * @ingroup ConcreteCommand
+ */
+
 class SendMessageCommand: public Command {
     public:
         SendMessageCommand(Users* fromUser, std::string message, ChatRoom* room);
         void execute();
         ~SendMessageCommand();
 };
+
+/**
+ * @class LogMessageCommand
+ * @brief Concrete command for logging a message.
+ * 
+ * @author Byron Pillay and Kiegan Lewis
+ * @date 2025-09-25
+ * @ingroup ConcreteCommand
+ */
 
 class LogMessageCommand: public Command {
     public:
