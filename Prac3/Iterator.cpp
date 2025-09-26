19e88de445603570d12185cec5492963addf0efc
Byron Pillay <byronpillay2704@gmail.com>
2025-09-26 12:01:09 +0200

    More doxygen comments

--- Prac3/Iterator.cpp
+++ Prac3/Iterator.cpp
@@ -1,5 +1,16 @@
 #include "Iterator.h"
 
+/**
+ * @brief Constructor for the `UserIterator` class.
+ * @details This is the constructor for the `UserIterator` class. 
+ * 1. It takes a list of `User` objects and sets `users` to this list.
+ * 2. It sets `start` to `list->begin()`.
+ * 3. It sets `end` to `list->end()`.
+ * 4. It sets `current` to `start`.
+ * 
+ * @param list This is a list of `User` objects.
+ */
+
 UserIterator::UserIterator(std::list<Users*>* list) {
     // Constructor implementation
     this->users = list;
@@ -8,9 +19,25 @@ UserIterator::UserIterator(std::list<Users*>* list) {
     this->current = start;
 }
 
+/**
+ * @brief Getter method for `start`.
+ * @details This method returns the 1st `Users` in the list.
+ * 
+ * @returns A reference to the 1st element in the list.
+ */
+
 Users* UserIterator::first(){
     return *this->start;
 }
+
+/**
+ * @brief Method to step through the list of `Users`.
+ * @details This is a method to step through the list of `Users`.
+ * 1. Checks if the current `Users` object is not the end of the list. If true, moves current to the next object in the list.
+ * 2. Checks if `current` is not `end`. If true, returns `current`. If false, returns `nullptr`.
+ * 
+ * @returns If, post increment, the object does not equal `end` then the function returns `*this->current`. Otherwise, returns `nullptr`.
+ */
 
 Users* UserIterator::next(){
     if(this->current != this->end) {
@@ -23,9 +50,20 @@ Users* UserIterator::next(){
     }
 }
 
+/**
+ * @brief Determines if the end of the list is reached.
+ * @returns Function returns `true` if current=end. Otherwise, returns `false`.
+ */
+
 bool UserIterator::isDone(){
     return this->current == this->end;
 }
+
+/**
+ * @brief Getter for `current`
+ * @returns This function returns `current` if `current` is not the `end`. Otherwise, returns `nullptr`.
+ */
+
 Users* UserIterator::currentItem(){
     if(this->current != this->end) {
         return *this->current;
@@ -34,20 +72,44 @@ Users* UserIterator::currentItem(){
     }
 }
 
+/**
+ * @brief Method to remove a `Users` object from the list.
+ */
+
 void UserIterator::remove(){
     if(this->current != this->end) {
         this->current = this->users->erase(this->current);
     }
 }
 
+/**
+ * @brief Destructor for the `UserIterator` class.
+ */
+
 UserIterator::~UserIterator() {
     users = nullptr;
 }
+
+/**
+ * @brief Constructor for the `ChatHistoryIterator` class.
+ * @details Sets the `history` member variable to the parameter `history`, and sets `current` to 0.
+ * 
+ * @param history A vector of strings, typically the messages of the a `ChatRoom`.
+ */
 
 ChatHistoryIterator::ChatHistoryIterator(std::vector<std::string>* history) {
     this->history = history;
     this->current = 0;
 }
+
+/**
+ * @brief Method to get the first string in the vector.
+ * @details
+ * 1. Checks if the vector `history` is empty. If true, the function returns an empty string to indicate that there is no chat history.
+ * 2. Otherwise, returns `(*this->history)[0](*this->history)[0]` which is the first location of the vector.
+ * 
+ * @returns This function returns and empty string if the vector `history` is empty. Otherwise, returns the first string of `history`.
+ */
 
 std::string ChatHistoryIterator::first(){
     if(this->history->empty()) {
@@ -55,6 +117,15 @@ std::string ChatHistoryIterator::first(){
     }
     return (*this->history)[0];
 }
+
+/**
+ * @brief Method to step through the vector
+ * @details 
+ * 1. Increments `current`.
+ * 2. Checks if `current` is less that the size of `history`. If true, returns `(*this->history)[this->current]`. Otherwise returns an empty string.
+ * 
+ * @returns If, post increment, current is within the size of `history` then the `current` location in the vector. Otherwise, returns an empty string.
+ */
 
 std::string ChatHistoryIterator::next(){
     ++this->current;
@@ -65,9 +136,19 @@ std::string ChatHistoryIterator::next(){
     }
 }
 
+/**
+ * @brief This method checks if the end of the vector has been reached.
+ * 
+ * @returns If `current` >= the size of vector `history` then the function returns true. Otherwise, the function returns false.
+ */
+
 bool ChatHistoryIterator::isDone(){
     return this->current >= this->history->size();
 }
+
+/**
+ * @brief Method to remove the `current` string from vector `history`.
+ */
 
 void ChatHistoryIterator::remove(){
     if(this->current < this->history->size()) {
@@ -78,6 +159,13 @@ void ChatHistoryIterator::remove(){
     }
 }
 
+/**
+ * @brief Method to get the `current` string in the vector.
+ * 
+ * @returns This method returns the `current` location in the vector if current is less than the vectors size. Otherwise, returns an empty string
+ * 
+ */
+
 std::string ChatHistoryIterator::currentItem(){
     if(this->current < this->history->size()) {
         return (*this->history)[this->current];
@@ -85,6 +173,10 @@ std::string ChatHistoryIterator::currentItem(){
         return "";
     }
 }
+
+/**
+ * @brief Destructor for the `ChatHistoryIterator` class.
+ */
 
 ChatHistoryIterator::~ChatHistoryIterator() {
     history = nullptr;
--- Prac3/Iterator.h
+++ Prac3/Iterator.h
@@ -6,12 +6,40 @@ class Users;
 #include <vector>
 #include <string>
 
+/**
+ * @defgroup Iterator Iterator Pattern
+ * @brief Collection of classes that participate in the Iterator design pattern
+ */
+
+/**
+ * @defgroup Concrete 
+ */
+
+ /**
+  * @class Iterator
+  * @brief This is the abstract Iterator class
+  * @details This is the abstract Iterator class which will be the base class for 
+  * @ingroup Iterator
+  */
+
 class Iterator {
     public:
         virtual bool isDone()=0;
         virtual void remove()=0;
         virtual ~Iterator() = default;
 };
+
+/**
+ * @defgroup ConcreteIterators
+ * @brief These are the concrete implementations of `Iterator`.
+ */
+
+/**
+ * @class UserIterator
+ * @brief Class to iterate over a list of Users.
+ * @details This class is responsible for providing an interface to step through a list of `Users` objects.
+ * @ingroup ConcreteIterators
+ */
 
 class UserIterator: public Iterator {
     private:
@@ -28,6 +56,13 @@ class UserIterator: public Iterator {
         Users* currentItem();
         ~UserIterator();
 };
+
+/**
+ * @class ChatHistoryIterator
+ * @brief Class to iterate over a vector of strings.
+ * @details This class is responsible for providing an interface to step through a vector of `message` strings from `Users`.
+ * @ingroup ConcreteIterators
+ */
 
 class ChatHistoryIterator: public Iterator {
     private:
